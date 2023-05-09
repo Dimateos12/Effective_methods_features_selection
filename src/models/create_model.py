c1d0c6c60d299de1475a66771464b3960551bd68
Mateusz <mateusz.jablonski12@gmail.com>
2023-05-09 10:13:52 +0200

    Fix error with RF

---
+++ src/models/create_model.py
@@ -0,0 +1,22 @@
+from sklearn.ensemble import RandomForestClassifier
+
+
+def create_model(n_estimators=100, max_depth=None):
+    """
+    Function that creates a random forest model with specified hyperparameters.
+
+    Parameters
+    ----------
+    n_estimators : int, optional (default=100)
+        Number of trees in the random forest.
+
+    max_depth : int or None, optional (default=None)
+        Maximum depth of a tree.
+
+    Returns
+    -------
+    model : RandomForestClassifier
+        Random forest model.
+    """
+    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+    return model
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,32 +1,31 @@
-from sklearn.ensemble import RandomForestClassifier
-
-
-def train_random_forest(
-        X_train, y_train, n_estimators=100, max_depth=None, random_state=None):
+def train_random_forest(X_train, y_train, rf_model, n_estimators=100, max_depth=None, random_state=None):
     """
     Trains a random forest classifier on the input training data.
 
     Parameters:
+    -----------
     X_train : numpy array
         A numpy array containing the features for the training data.
     y_train : numpy array
         A numpy array containing the labels for the training data.
-    n_estimators : int, optional
-        The number of trees in the forest. Default is 100.
-    max_depth : int or None, optional
-        The maximum depth of the tree. Default is None.
-    random_state : int or None, optional
-        Seed for random number generation. Default is None.
+    rf_model : RandomForestClassifier object
+        A RandomForestClassifier object to be trained on the input data.
+    n_estimators : int, optional (default=100)
+        The number of trees in the forest.
+    max_depth : int or None, optional (default=None)
+        The maximum depth of the tree.
+    random_state : int or None, optional (default=None)
+        Seed for random number generation.
 
     Returns:
+    --------
     rf_model : RandomForestClassifier object
         A trained random forest classifier model.
     """
 
     # Initialize a random forest classifier with the specified
     # hyperparameters
-
-    rf_model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=random_state)
+    rf_model.set_params(n_estimators=n_estimators, max_depth=max_depth, random_state=random_state)
 
     # Train the model on the input data
     rf_model.fit(X_train, y_train)
